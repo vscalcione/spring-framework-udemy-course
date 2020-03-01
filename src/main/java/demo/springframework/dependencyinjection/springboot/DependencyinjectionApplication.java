289fc79e79dbbfebb2f5ab1c41149a3f696d4086
vscalcione <vincenzo99.scalcione@gmail.com>
2020-03-01 19:36:31 +0100

    Moved file DependencyInjectionApplication.java to another external package and add the @ComponentScan annotation

--- src/main/java/demo/springframework/dependencyinjection/springboot/DependencyinjectionApplication.java
+++ src/main/java/demo/springframework/dependencyinjection/springboot/DependencyinjectionApplication.java
@@ -1,12 +1,14 @@
-package demo.springframework.dependencyinjection;
+package demo.springframework.dependencyinjection.springboot;
 
 
 import demo.springframework.dependencyinjection.controllers.GreetingController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("demo.springframework.dependencyinjection")
 public class DependencyinjectionApplication {
 
     public static void main(String[] args) {
